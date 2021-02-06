321582b94e03a1e1992be443c6065558c5d05e60
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2021-02-05 21:36:27 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 /* Christopher Bernhard
 ICT 4510
-2/14/2021 */
-
+2/14/2021
+username: cbernhard
+password: ict4510@pp
+The web page takes a username and password, if correct returns an user object and allows the user to 
+proceed.
+*/
 
 "use strict"
 
@@ -11,9 +15,9 @@ const messageContainer = document.querySelector('#message-container');
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
-  const userName = document.querySelector('#username').value;
+  const username = document.querySelector('#username').value;
   const password = document.querySelector('#password').value;
-  const url = 'https://jsonplaceholder.typicode.com/posts';
+  const url = 'https://ict4510.herokuapp.com/api/login';
 
   const formPostData = {
     method: 'POST',
@@ -21,7 +25,7 @@ loginForm.addEventListener('submit', (e) => {
       'Content-type': 'application/json; charset=UTF-8',
     },
     body: JSON.stringify({
-      userName: userName,
+      username: username,
       password: password
     }),
   };
@@ -36,20 +40,17 @@ loginForm.addEventListener('submit', (e) => {
     })
     .then((user) => {
       sessionStorage.setItem('user', JSON.stringify(user));
-      console.log(user);
       displayDataHtml();
     })
     .catch(error => {
       console.log(error, 'there was an error')
     });
-
 })
-
 
 const displayDataHtml = () => {
   loginForm.classList.add('invisible');
-  const userName = JSON.parse(sessionStorage.getItem('user')).userName;
-  messageContainer.innerHTML = `<p class="fs-2 text-center">Hello ${userName}</p>
+  const firstName = JSON.parse(sessionStorage.getItem('user')).user.first_name;
+  messageContainer.innerHTML = `<p class="fs-2 text-center">Hello ${firstName}</p>
   <p class="fs-2 text-center mb-4">You May Enter The Record Exchange</p>
   <a href="#" class="text-center btn btn-outline-primary fs-4" id="my-button">Enter</a>
   `;
